53eaee2cb3f5af6754669f27dc12cf8944f4b91f
Strong <strong@theia.tw>
2020-01-20 10:54:35 +0800

    unitest

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,30 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-      MaterialApp(
-          home: Scaffold(
-            backgroundColor: Colors.greenAccent[100],
-            appBar:  AppBar(
-              title: Center(
-                child: Text("第一支 Flutter APP"),
-              ),
-              backgroundColor: Colors.blueGrey[700],
-            ),
-            body: Center(
-              child: Image(
-//                image: NetworkImage("https://i.ytimg.com/vi/Iyqf-xbMfy0/maxresdefault.jpg")
-                image: AssetImage("images/mafuyu.jpg"),
-              ),
-            ),
-          ),
-      ),
-  );
+  runApp(MyApp());
 }
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.greenAccent[100],
+        appBar:  AppBar(
+          title: Center(
+            child: Text("第一支 Flutter APP"),
+          ),
+          backgroundColor: Colors.blueGrey[700],
+        ),
+        body: Center(
+          child: Image(
+//                image: NetworkImage("https://i.ytimg.com/vi/Iyqf-xbMfy0/maxresdefault.jpg")
+            image: AssetImage("images/mafuyu.jpg"),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
